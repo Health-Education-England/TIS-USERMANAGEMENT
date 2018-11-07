ee04ed182e3a246967b178ae0e7d6741be437c15
paulhoang-hee <paul.hoang@hee.nhs.uk>
2018-11-07 09:53:32 +0000

    increase the amount of time before hystrix fails when talking to profile to 5 seconds

--- src/main/java/uk/nhs/hee/tis/usermanagement/command/profile/ProfileHystrixCommand.java
+++ src/main/java/uk/nhs/hee/tis/usermanagement/command/profile/ProfileHystrixCommand.java
@@ -7,13 +7,13 @@ import com.netflix.hystrix.HystrixCommandProperties;
 public abstract class ProfileHystrixCommand<T> extends HystrixCommand<T> {
 
   private static final String COMMAND_KEY = "PROFILE_COMMANDS";
-  private static final int TWO_SECOND_TIMEOUT_IN_MILLIS = 2000;
+  private static final int FIVE_SECOND_TIMEOUT_IN_MILLIS = 5000;
 
   public ProfileHystrixCommand() {
     super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(COMMAND_KEY))
         .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
             .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.SEMAPHORE)
-            .withExecutionTimeoutInMilliseconds(TWO_SECOND_TIMEOUT_IN_MILLIS))
+            .withExecutionTimeoutInMilliseconds(FIVE_SECOND_TIMEOUT_IN_MILLIS))
     );
   }
 }
