785dabda1107b0c94b30702d61d8003f3f8cc650
paul.hoang <agent_paul@hotmail.com>
2018-12-13 23:13:56 +0000

    missed out async config as all events were being processed on the same request thread

---
+++ src/main/java/uk/nhs/hee/tis/usermanagement/config/AsyncConfiguration.java
@@ -0,0 +1,32 @@
+package uk.nhs.hee.tis.usermanagement.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AsyncConfiguration implements AsyncConfigurer {
+
+  private final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
+
+  /**
+   * Bean to handle application events asynchronously
+   *
+   * @return
+   */
+  @Bean(name = "applicationEventMulticaster")
+  public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+    SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+    eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+    return eventMulticaster;
+  }
+}
