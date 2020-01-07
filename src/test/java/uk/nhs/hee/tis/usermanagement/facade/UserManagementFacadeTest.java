25300e6548783e8148edc4b678db0b2d49e82e93
Andy Dingley <judge40@gmail.com>
2020-01-07 14:52:01 +0000

    Create tests for getting username ignoring case

--- src/test/java/uk/nhs/hee/tis/usermanagement/facade/UserManagementFacadeTest.java
+++ src/test/java/uk/nhs/hee/tis/usermanagement/facade/UserManagementFacadeTest.java
@@ -1,17 +1,24 @@
 package uk.nhs.hee.tis.usermanagement.facade;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import com.transformuk.hee.tis.profile.service.dto.HeeUserDTO;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-import com.google.common.collect.Lists;
-import uk.nhs.hee.tis.usermanagement.service.KeyCloakAdminClientService;
+import uk.nhs.hee.tis.usermanagement.DTOs.UserDTO;
+import uk.nhs.hee.tis.usermanagement.mapper.HeeUserMapper;
 import uk.nhs.hee.tis.usermanagement.service.ProfileService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,16 +27,16 @@ public class UserManagementFacadeTest {
   private String adminRole = "HEE TIS Admin", etlRole = "ETL", roRole = "RVOfficer", rvAdmin = "RVAdmin";
   @InjectMocks
   UserManagementFacade testClass;
-  
+
   @Mock
   ProfileService profileService;
-  
-  @Mock
-  KeyCloakAdminClientService keyCloakAdminClientService;
+
+  @Spy
+  private HeeUserMapper userMapper;
 
   @Test
   public void testGetAllAssignableRoles() {
-    List mockRoles = Lists.newArrayList(adminRole, rvAdmin, roRole , etlRole);
+    List mockRoles = Lists.newArrayList(adminRole, rvAdmin, roRole, etlRole);
 
     when(profileService.getAllRoles()).thenReturn(mockRoles);
     List<String> actual = testClass.getAllAssignableRoles();
@@ -37,4 +44,41 @@ public class UserManagementFacadeTest {
     verify(profileService).getAllRoles();
   }
 
+  /**
+   * Test that null is returned when the user is not found.
+   */
+  @Test
+  public void testGetUserByNameIgnoreCase_userNotFound_null() {
+    // Record expectations.
+    when(profileService.getUserByUsernameIgnoreCase("testUser_1")).thenReturn(Optional.empty());
+
+    // Call the code under test.
+    UserDTO user = testClass.getUserByNameIgnoreCase("testUser_1");
+
+    // Perform assertions.
+    assertThat("The user did not match the expected value.", user, nullValue());
+  }
+
+  /**
+   * Test that the user is returned when the user is found.
+   */
+  @Test
+  public void testGetUserByNameIgnoreCase_userFound_user() {
+    // Set up test data.
+    HeeUserDTO heeUser = new HeeUserDTO();
+    heeUser.setName("testUser_1");
+
+    // Record expectations.
+    when(profileService.getUserByUsernameIgnoreCase("testUser_1")).thenReturn(Optional.of(heeUser));
+
+    // Call the code under test.
+    UserDTO user = testClass.getUserByNameIgnoreCase("testUser_1");
+
+    // Perform assertions.
+    assertThat("The user's name did not match the expected value.", user.getName(),
+        is("testUser_1"));
+
+    // Verify expectations.
+    verify(userMapper).convert(heeUser);
+  }
 }
